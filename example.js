06ce687adb3bfa8e37ad30258653d0e819ba29ed
TalitaK564 <talita.kauane@escolar.ifrn.edu.br>
2026-09-14 23:58:49 +0000

    feat: usando operador ternario para definir titulo padrao no Header

--- example.js
+++ example.js
@@ -1,11 +1,11 @@
-function createTitle(title) {
-  if (title) {
-    return title;
-  } else {
-    return 'Default title';
-  }
+function Header({ title }) {
+  return <h1>{title ? title : 'Default title'}</h1>;
 }
  
-function Header({ title }) {
-  return <h1>{createTitle(title)}</h1>;
+function HomePage() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
